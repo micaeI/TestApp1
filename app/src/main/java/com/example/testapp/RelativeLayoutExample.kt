5c647d7d317c7552f81b885b0a1def1e014cd99a
micael <97756757+micaeI@users.noreply.github.com>
2024-10-08 12:02:05 -0300

    Update RelativeLayoutExample.kt
    botao

--- app/src/main/java/com/example/testapp/RelativeLayoutExample.kt
+++ app/src/main/java/com/example/testapp/RelativeLayoutExample.kt
@@ -3,6 +3,8 @@ package com.example.testapp
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 import com.example.testapp.databinding.RelativeLayoutExampleBinding
+import kotlin.random.Random
+import android.widget.Toast
 
 class RelativeLayoutExample: AppCompatActivity() {
 
@@ -13,5 +15,16 @@ class RelativeLayoutExample: AppCompatActivity() {
 
         binding = RelativeLayoutExampleBinding.inflate(layoutInflater)
         setContentView(binding.root)
+
+         binding.button1.setOnClickListener {
+            val multipleOfFive = generateMultipleOfFive(1, 100)
+            Toast.makeText(this, "Múltiplo de 5 gerado: $multipleOfFive", Toast.LENGTH_SHORT).show()
+            binding.textView1.text = "Múltiplo de 5 gerado: $multipleOfFive"
+        }
+    }
+    
+    private fun generateMultipleOfFive(start: Int, end: Int): Int {
+        val randomNumber = Random.nextInt(start, end)
+        return (randomNumber / 5) * 5 // Gera o múltiplo de 5 mais próximo do número aleatório gerado
     }
 }
